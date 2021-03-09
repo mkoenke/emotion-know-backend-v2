8f2cd7adbecdac0623c3d87c86d5b75b47e13d5d
mkoenke <mkoenke@sas.upenn.edu>
2021-03-09 14:01:26 -0500

    change to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,12 +24,12 @@ VideoEntry.destroy_all
 puts "destroying video entries"
 
 
-# parent = Parent.create!(email: "mkoenke@sas.upenn.edu", password_digest: BCrypt::Password.create("1234"))
+parent = Parent.create!(email: "mkoenke@sas.upenn.edu", password_digest: BCrypt::Password.create("1234"))
 
-# puts "Parent seeded"
+puts "Parent seeded"
 
-# child = Child.create!(username: "Angelina", password_digest: BCrypt::Password.create("1234"), image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.1YM53mG10H_U25iPjop83QHaEo%26pid%3DApi&f=1", parent_id: parent.id)
-# puts "Child seeded"
+child = Child.create!(username: "Angelina", password_digest: BCrypt::Password.create("1234"), image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.1YM53mG10H_U25iPjop83QHaEo%26pid%3DApi&f=1", parent_id: parent.id)
+puts "Child seeded"
 
 # journal_entry = JournalEntry.create!(title: "Day 1", child_id: child.id, content: "Today was a good day")
 # puts "Journal seeded"
