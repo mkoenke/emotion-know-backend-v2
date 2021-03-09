a015e57bda536fd090594ddb3fcc95f378329baa
dyson1602 <cclark000@citymail.cuny.edu>
2021-03-09 14:13:36 -0500

    removed: has_many audio and journal from parent and child serializers

--- app/serializers/child_serializer.rb
+++ app/serializers/child_serializer.rb
@@ -1,8 +1,6 @@
 class ChildSerializer < ActiveModel::Serializer
   attributes :id, :username, :image, :parent_email, :parent
 
-  has_many :journal_entries
-  has_many :audio_entries
   has_many :video_entries
 
   def parent_email
