b2ed728007c73e822d18a6707881b6055ae70188
dyson1602 <cclark000@citymail.cuny.edu>
2021-05-27 17:36:59 -0400

    changed: video_entries to video_reports so only reports load upon login

--- app/serializers/child_serializer.rb
+++ app/serializers/child_serializer.rb
@@ -1,8 +1,7 @@
 class ChildSerializer < ActiveModel::Serializer
   attributes :id, :username, :name, :image, :parent_email, :parent
 
-  # Commented out to test video fetch limit upon login
-  # has_many :video_entries
+  has_many :video_reports
 
   def parent_email
     self.object.parent.email
