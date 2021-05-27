77ede6f963e79926455484294d1ecdc43f31bd3a
dyson1602 <cclark000@citymail.cuny.edu>
2021-05-27 17:01:30 -0400

    removed: video_entries from serializer. videos will no longer all auto-load upon login

--- app/serializers/child_serializer.rb
+++ app/serializers/child_serializer.rb
@@ -1,7 +1,8 @@
 class ChildSerializer < ActiveModel::Serializer
   attributes :id, :username, :name, :image, :parent_email, :parent
 
-  has_many :video_entries
+  # Commented out to test video fetch limit upon login
+  # has_many :video_entries
 
   def parent_email
     self.object.parent.email
