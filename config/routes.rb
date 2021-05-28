e794047338a5a3dd9af7376a032e234d61f67e77
mkoenke <mkoenke@sas.upenn.edu>
2021-05-28 14:39:43 -0400

    add route for forgot_child_password

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post '/login', to: 'auth#create'
   post '/parentLogin', to: 'auth#create_parent'
   post '/forgot_password', to: "passwords#forgot"
+  post '/forgot_child_password', to: "passwords#forgot_child"
   post '/reset_password', to: "passwords#reset"
   get 'get_current_parent', to: "sessions#get_current_parent"
 
